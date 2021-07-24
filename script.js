e5b96f3540fbf4bcf16a0f62cf7acdb1ab1a3ac6
Kadugs <cadu20.gomes@gmail.com>
2021-07-24 18:37:17 -0300

    adicionada soma ao selecionar opcoes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let vistoSelecao;
+let vistoSelecao = 0;
+let valorPrato = 0;
+let valorBebida = 0;
+let valorSobremesa = 0;
+let valorTotal = 0;
 
+//cria e remove a marcação dos itens
 function adicionaMarca(classeDeSelecao, itemSelecionado) {
     let encontraSelecao = document.querySelector(`.${classeDeSelecao}`);
     if(encontraSelecao !== null) {
@@ -12,19 +17,27 @@ function adicionaMarca(classeDeSelecao, itemSelecionado) {
     vistoSelecao.classList.remove("escondido");
 }
 
+//pega o valor do item escolhido
+function selecionaValor(produto) {
+    let valor = produto.querySelector(".valor").innerHTML;
+    return Number(valor);
+}
+
+//funções disparadas ao clicar
 function selecionaPrato(itemSelecionado) {
-    adicionaMarca('prato-selecionado', itemSelecionado);  
+    adicionaMarca('prato-selecionado', itemSelecionado);
+    valorPrato = selecionaValor(itemSelecionado);
 }
 function selecionaBebida(itemSelecionado) {
-    adicionaMarca('bebida-selecionada', itemSelecionado);  
+    adicionaMarca('bebida-selecionada', itemSelecionado); 
+    valorBebida = selecionaValor(itemSelecionado);
 }
 function selecionaSobremesa(itemSelecionado) {
-    adicionaMarca('sobremesa-selecionada', itemSelecionado);  
+    adicionaMarca('sobremesa-selecionada', itemSelecionado);
+    valorSobremesa = selecionaValor(itemSelecionado);
 }
 
-
-///usar algo = valor.innerHTML p pegar o valor
-
+//verifica se todos os itens foram esconlhidos para liberar a confirmação
 function verificaSelecao() {
     const pratoSelecionado = document.querySelector(".prato-selecionado");
     const bebidaSelecionada = document.querySelector(".bebida-selecionada");
@@ -32,16 +45,18 @@ function verificaSelecao() {
     const botao = document.querySelector(".botao-confirmar"); 
     const textoBotao = document.querySelector(".botao-confirmar p");
 
-    if(pratoSelecionado && bebidaSelecionada && sobremesaSelecionada) {
+    if(pratoSelecionado !== null && bebidaSelecionada !== null  && sobremesaSelecionada !== null) {
         botao.classList.add("liberar-confirmacao");
         textoBotao.innerHTML = "Fechar pedido";
     }
 }
 
+//disparada ao clicar em Fechar Pedido
 function confirmarPedido() {
+    valorTotal = valorPrato + valorBebida + valorSobremesa;
     const botao = document.querySelector(".botao-confirmar");
     if(botao.classList.contains("liberar-confirmacao")) {
-        const mensagem = encodeURIComponent( `Olá, gostaria de fazer o pedido: \n - Prato: moqueca \n - Bebida: suco de laranja \n - Sobremesa: doce de banana \n Valor 300`);
-        window.open(`https://wa.me/5533988538985?text=${mensagem}`);
+        const mensagem = encodeURIComponent( `Olá, gostaria de fazer o pedido: \n - Prato: moqueca \n - Bebida: suco de laranja \n - Sobremesa: doce de banana \n Valor: R$ ${valorTotal.toFixed(2)}`);
+        window.open(`https://wa.me/5533988418125?text=${mensagem}`);
     }
 }
